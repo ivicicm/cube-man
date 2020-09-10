849ee27a506ff0d8f4efe0b5cb505bd988ad3ab6
Michal Ivičič <michaljivicic@gmail.com>
2020-09-10 16:19:41 +0200

    + game level page

---
+++ src/pages/GameLevel.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './GameLevel.module.css'
+
+function App() {
+  return (
+    <div className={styles.map}>
+      
+    </div>
+  );
+}
+
+export default App;
