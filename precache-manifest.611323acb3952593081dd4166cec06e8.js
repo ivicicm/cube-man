f312a63ed44433c7bc186c160a1ea53dcdc28e0a
Michal Ivičič <michaljivicic@gmail.com>
2020-10-11 17:46:10 +0200

    Updates

---
+++ precache-manifest.611323acb3952593081dd4166cec06e8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dc963bd39ed70bc09766c8b0c8c26f4",
+    "url": "/cube-man/index.html"
+  },
+  {
+    "revision": "5f6df113d97719747f5b",
+    "url": "/cube-man/static/css/main.72a52174.chunk.css"
+  },
+  {
+    "revision": "54ec124d1948fa630063",
+    "url": "/cube-man/static/js/2.685638d5.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/cube-man/static/js/2.685638d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f6df113d97719747f5b",
+    "url": "/cube-man/static/js/main.8d9d5261.chunk.js"
+  },
+  {
+    "revision": "1aa340b66f38f5c597ae",
+    "url": "/cube-man/static/js/runtime-main.b87cafb6.js"
+  },
+  {
+    "revision": "acb7781675f50735140de0547fa0b58d",
+    "url": "/cube-man/static/media/brickWall.acb77816.svg"
+  },
+  {
+    "revision": "bcc8edaff3c01429ca188185528bdf31",
+    "url": "/cube-man/static/media/cube.bcc8edaf.svg"
+  },
+  {
+    "revision": "95bca3c5bf95217e79d0117883e5b662",
+    "url": "/cube-man/static/media/cubeman.95bca3c5.png"
+  },
+  {
+    "revision": "0a0764e90245c14310e8bbb8350f74d6",
+    "url": "/cube-man/static/media/drill.0a0764e9.png"
+  },
+  {
+    "revision": "deabd352fbe5e23dc9b08e2539baa428",
+    "url": "/cube-man/static/media/star.deabd352.svg"
+  }
+]);
